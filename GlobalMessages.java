455c00ae709310aea70f8246f6510d7c948a0338
0xJayden <93624727+0xJayden@users.noreply.github.com>
2021-12-16 21:02:33 -0800

    Add files via upload

---
+++ GlobalMessages.java
@@ -0,0 +1,83 @@
+package com.vencillio.rs2.content;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.vencillio.core.task.Task;
+import com.vencillio.core.task.TaskQueue;
+import com.vencillio.core.util.Utility;
+import com.vencillio.rs2.entity.World;
+
+/**
+ * Handles the global messages
+ * @author Daniel
+ *
+ */
+public class GlobalMessages {
+	
+	/**
+	 * The logger for the class
+	 */
+	private static Logger logger = Logger.getLogger(GlobalMessages.class.getSimpleName());
+
+	/**
+	 * The news color text
+	 */
+	private static String newsColor = "<col=013B4F>";
+
+	/**
+	 * The news icon
+	 */
+	private static String iconNumber = "<img=8>";
+	
+	/**
+	 * Holds all the announcements in a arraylist
+	 */
+	public final static ArrayList<String> announcements = new ArrayList<String>();
+
+	/**
+	 * The random messages that news will send
+	 */
+	public static final String[] ANNOUNCEMENTS = { 
+		"Talk to the guard at Edgeville bank to add extra security to your account!",
+		"Read up on the rules of Web3Scape to ensure safe and enjoyable game play!",
+		"Do you love Web3Scape? Please show us some love by donating!",
+		"Want to disable TriviaBot? Or add a notification bar? ::triviasettings",
+		"Considering donating? Donors have access to many custom content!",
+		"Considering donating? Donors will be given membership status.",
+		"Members can change their title by speaking to Dunce at Memberzone.",
+		"Having an issue with your account? Tell us about it on discord.",
+		"Do you have an interesting idea? Suggest it to us in discord!",
+		"Found a bug? Report it on discord and we will fix it for you!",
+		"Did you know you can talk to Big Mo at home for a title?",
+		"Our website is located at http://www.Web3Scape.com!",
+	};
+	
+	/**
+	 * Declares all the announcements
+	 */
+	public static void declare() {
+		for (int i = 0; i < ANNOUNCEMENTS.length; i++) {
+			announcements.add(ANNOUNCEMENTS[i]);
+		}
+		logger.info(Utility.format(announcements.size()) + " Announcements have been loaded successfully.");
+	}
+
+	/**
+	 * Initializes the task
+	 */
+	public static void initialize() {
+		TaskQueue.queue(new Task(250, false) {
+			@Override
+			public void execute() {
+				final String announcement = Utility.randomElement(announcements);
+				World.sendGlobalMessage(iconNumber + newsColor + " " + announcement);
+			}
+
+			@Override
+			public void onStop() {
+			}
+		});
+	}
+	
+}
